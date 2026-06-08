b0997bbb4fbb4f756dd7c6e40057cff3a5860e27
Asad-gove <asadgove@gmail.com>
2026-06-08 11:25:50 +0530

    Update kruskals.c

--- ADALAB/kruskals/kruskals.c
+++ ADALAB/kruskals/kruskals.c
@@ -1,43 +1,61 @@
 #include <stdio.h>
 
-void Floyds(int n, int cost[50][50], int A[50][50])
-{
-    int i, j, k;
+int parent[20];
 
-    for(i = 1; i <= n; i++)
-        for(j = 1; j <= n; j++)
-            A[i][j] = cost[i][j];
+int find(int i) {
+    while (parent[i])
+        i = parent[i];
+    return i;
+}
 
-    for(k = 1; k <= n; k++)
-        for(i = 1; i <= n; i++)
-            for(j = 1; j <= n; j++)
-                if(A[i][k] + A[k][j] < A[i][j])
-                    A[i][j] = A[i][k] + A[k][j];
+int uni(int i, int j) {
+    if (i != j) {
+        parent[j] = i;
+        return 1;
+    }
+    return 0;
 }
 
 int main() {
-    int n, i, j;
-    int cost[50][50], A[50][50];
+    int n, cost[20][20];
+    int mincost = 0, edges = 0;
+
     printf("Enter number of vertices: ");
     scanf("%d", &n);
-    printf("Enter the cost matrix (use 999 for infinity):\n");
-    for(i = 1; i <= n; i++) {
-        for(j = 1; j <= n; j++) {
+
+    printf("Enter cost matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             scanf("%d", &cost[i][j]);
-            if (cost[i][j] == 0 && i != j) {
-                cost[i][j] = 999;
+
+    printf("Edges in MST:\n");
+
+    while (edges < n - 1) {
+        int min = 9999, a = -1, b = -1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (cost[i][j] < min) {
+                    min = cost[i][j];
+                    a = i;
+                    b = j;
+                }
             }
         }
-    }
-    Floyds(n, cost, A);
-    printf("\nThe all-pairs shortest path matrix is:\n");
-    for(i = 1; i <= n; i++) {
-        for(j = 1; j <= n; j++) {
-            if (A[i][j] == 999) printf("INF ");
-            else printf("%d  ", A[i][j]);
+
+        int u = find(a);
+        int v = find(b);
+
+        if (uni(u, v)) {
+            printf("%d - %d : %d\n", a, b, min);
+            mincost += min;
+            edges++;
         }
-        printf("\n");
+
+        cost[a][b] = cost[b][a] = 9999;
     }
+
+    printf("Minimum Cost = %d\n", mincost);
 
     return 0;
 }
